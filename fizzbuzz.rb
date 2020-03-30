0e79199f0fbe50270cea34c60aef6c99d2e5e180
Harish Manoharan <harishmanoharan17@gmail.com>
2020-03-30 09:19:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num%3==0
-    "Fizz"
+    if num%5==0
+      "FizzBuzz"
+    else
+      "Fizz"
+    end
+  elsif num%5==0
+    "Buzz"
   end
 end
